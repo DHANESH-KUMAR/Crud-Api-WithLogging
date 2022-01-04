e2c3fb906c3705704165eb5b92b57e33b299bb22
Dhanesh <DHANESH-KUMAR@HOTMAIL.COM>
2022-01-03 21:05:00 -0800

    Request And Response Logger Fix

--- school/school.app/Filters/RequestResponseLoggerAttribute.cs
+++ school/school.app/Filters/RequestResponseLoggerAttribute.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
 using Microsoft.IO;
+using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -15,65 +16,36 @@ namespace school.app.Filters
         {
             this._logger = logger;
         }
-        public async void OnActionExecuting(ActionExecutingContext context)
+        public  void OnActionExecuting(ActionExecutingContext context)
         {
-            await LogRequest(context.HttpContext);
-        }
+            var request = context.HttpContext.Request;
+            var jsonString = JsonConvert.SerializeObject(context.Result);
 
-        private async Task LogRequest(HttpContext context)
-        {
-            context.Request.EnableBuffering();
-            RecyclableMemoryStreamManager _recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
-            await using var requestStream = _recyclableMemoryStreamManager.GetStream();
-            await context.Request.Body.CopyToAsync(requestStream);
             _logger.LogInformation($"Http Request Information:{Environment.NewLine}" +
-                                   $"Schema:{context.Request.Scheme} " +
-                                   $"Host: {context.Request.Host} " +
-                                   $"Path: {context.Request.Path} " +
-                                   $"QueryString: {context.Request.QueryString} " +
-                                   $"Request Body: {ReadStreamInChunks(requestStream)}");
-            context.Request.Body.Position = 0;
+                                    $"Schema:{request.Scheme} " +
+                                    $"Host: {request.Host} " +
+                                    $"Path: {request.Path} " +
+                                    $"QueryString: {request.QueryString} " +
+                                    $"Request Body: {jsonString}");
         }
 
-        public async void OnActionExecuted(ActionExecutedContext context)
+        
+
+        public  void OnActionExecuted(ActionExecutedContext context)
         {
-          //  await LogResponse(context.HttpContext);
+            var request = context.HttpContext.Request;
+            var jsonString = JsonConvert.SerializeObject(context.Result);
 
-        }
-
-        private async Task LogResponse(HttpContext context)
-        {
-            RecyclableMemoryStreamManager _recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
-            var originalBodyStream = context.Response.Body;
-            await using var responseBody = _recyclableMemoryStreamManager.GetStream();
-            context.Response.Body = responseBody;
-
-            context.Response.Body.Seek(0, SeekOrigin.Begin);
-            var text = await new StreamReader(context.Response.Body).ReadToEndAsync();
-            context.Response.Body.Seek(0, SeekOrigin.Begin);
             _logger.LogInformation($"Http Response Information:{Environment.NewLine}" +
-                                   $"Schema:{context.Request.Scheme} " +
-                                   $"Host: {context.Request.Host} " +
-                                   $"Path: {context.Request.Path} " +
-                                   $"QueryString: {context.Request.QueryString} " +
-                                   $"Response Body: {text}");
-            await responseBody.CopyToAsync(originalBodyStream);
+                                    $"Schema:{request.Scheme} " +
+                                    $"Host: {request.Host} " +
+                                    $"Path: {request.Path} " +
+                                    $"QueryString: {request.QueryString} " +
+                                    $"Request Body: {jsonString}");
+
         }
-        private string ReadStreamInChunks(Stream stream)
-        {
-            const int readChunkBufferLength = 4096;
-            stream.Seek(0, SeekOrigin.Begin);
-            using var textWriter = new StringWriter();
-            using var reader = new StreamReader(stream);
-            var readChunk = new char[readChunkBufferLength];
-            int readChunkLength;
-            do
-            {
-                readChunkLength = reader.ReadBlock(readChunk, 0, readChunkBufferLength);
-                textWriter.Write(readChunk, 0, readChunkLength);
-            } while (readChunkLength > 0);
-            return textWriter.ToString();
-        }
+
+      
 
     }
 }
